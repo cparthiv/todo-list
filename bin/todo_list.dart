967476d7c819f2342a4af0adf8dff6d4838872b5
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 11:36:03 -0700

    save tasks to file added to bin

--- bin/todo_list.dart
+++ bin/todo_list.dart
@@ -1,10 +1,31 @@
 import 'package:todo_list/todo_list.dart' as todo_list;
+import 'dart:io';
 
 void main(List<String> arguments) {
   List<String> tasks = [];
   while (true) {
-    todo_list.addTask(tasks);
     print("\x1B[2J\x1B[0;0H");
     todo_list.viewTasks(tasks);
+    print('---------------');
+    print('Options: ');
+    print('1: Add task');
+    print('2: Delete task');
+    print('3: Save tasks to file');
+    print('4: Exit program');
+
+    String? option = stdin.readLineSync();
+    switch (option) {
+      case '1':
+        todo_list.addTask(tasks);
+        continue;
+      case '2':
+        todo_list.deleteTasks(tasks);
+        continue;
+      case '3':
+        todo_list.saveTasksToFile(tasks);
+        continue;
+      case '4':
+        break;
+    }
   }
 }
