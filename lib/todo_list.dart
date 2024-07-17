17a775d6523b106397479991f28fd2edaa9d870d
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 10:54:40 -0700

    add task and view task

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,3 +1,21 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+List<String> addTask(List<String> initialTasks) {
+  print('Enter a task to add to the list: ');
+  String? inputtedTask = stdin.readLineSync();
+  if (inputtedTask != null && inputtedTask.isNotEmpty) {
+    initialTasks.add(inputtedTask);
+  } else {
+    print('Nothing has been inputted, returning.');
+  }
+  return initialTasks;
+}
+
+void viewTasks(List<String> tasks) {
+  List<int> taskIndexes = List.generate(tasks.length, (int index) => index + 1);
+  print('Tasks: ');
+  for (final taskIndex in taskIndexes) {
+    print('$taskIndex: ${tasks[taskIndex - 1]}');
+  }
+  print('\n');
 }
