dbaaa42bde0dda7b0af251bfb412016a92031909
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 10:54:32 -0700

    basic add func

--- bin/todo_list.dart
+++ bin/todo_list.dart
@@ -1,5 +1,11 @@
 import 'package:todo_list/todo_list.dart' as todo_list;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${todo_list.calculate()}!');
+  List<String> tasks = [];
+  while (true) {
+    todo_list.addTask(tasks);
+    print("\x1B[2J\x1B[0;0H");
+    todo_list.viewTasks(tasks);
+  }
 }
