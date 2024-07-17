c387409716d544e86829d9e6a418d8d1c3fb7b63
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 10:37:53 -0700

    remove tests for now

--- test/todo_list_test.dart
+++ test/todo_list_test.dart
@@ -1,8 +1,9 @@
-import 'package:todo_list/todo_list.dart';
-import 'package:test/test.dart';
+// import 'package:todo_list/todo_list.dart';
+// import 'package:test/test.dart';
 
 void main() {
+  /*
   test('calculate', () {
     expect(calculate(), 42);
-  });
+  });*/
 }
